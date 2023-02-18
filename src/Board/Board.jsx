47eb7cd2876090709dd7169cece7f2c594af783a
edsontomas54 <donthimas54@gmail.com>
2023-02-18 20:18:13 +0200

    first commit

---
+++ src/Board/Board.jsx
@@ -0,0 +1,20 @@
+import {useEffect, useRef} from 'react';
+
+import './Board.css';
+
+/**
+ * TODO: add a more elegant UX for before a game starts and after a game ends.
+ * A game probably shouldn't start until the user presses an arrow key, and
+ * once a game is over, the board state should likely freeze until the user
+ * intentionally restarts the game.
+ */
+
+
+
+function Board() {
+  return (
+    <div>Board</div>
+  )
+}
+
+export default Board
